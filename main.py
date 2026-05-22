aa68e7725b59b7eb9a7a90a8065363e769dae988
Revanth <isaacrevanth@gmail.com>
2026-05-22 21:56:32 +0530

    Added production CORS

--- main.py
+++ main.py
@@ -31,7 +31,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "http://localhost:3000",
+    "https://digit-ai-rbv.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
